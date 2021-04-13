c1d0ca630858cbf4b1f39cfa8ee2eb706fc919bb
renateaskevold <renate@spektrum.no>
2021-04-13 11:16:07 +0200

    1e

--- Assignment 3/task_1_data1.py
+++ Assignment 3/task_1_data1.py
@@ -6,11 +6,12 @@ data = np.loadtxt("data1.txt")
 #data2 = np.loadtxt("data2.txt")
 #data3 = np.loadtxt("data3.txt")
 
+## Task 1d
 data_len = np.size(data)
 ones = 0
 zerohs = 0
 
-for i in range(np.size(data)):
+for i in range(data_len):
     if data[i] == 1:
         ones = ones + 1
     else:
@@ -29,3 +30,27 @@ print("P_Zerohs ", p_zerohs)
 
 H = -p_zerohs*np.log2(p_zerohs) - (p_ones)*np.log2(p_ones)
 print("The entropy is for data1 is ", H)
+
+## Task 1e
+data_2bit = []
+oneone = 0
+onezero = 0
+zeroone = 0
+zerozero = 0
+
+for i in range(0,data_len,2):
+    if data[i] == 1:
+        if data[i+1] == 1:
+            oneone += 1
+        elif data[i+1] == 0:
+            onezero += 1
+    else:
+        if data[i+1] == 1:
+            zeroone += 1
+        elif data[i+1] == 0:
+            zerozero += 1
+
+print(f"Number of 11s:{oneone}")
+print(f"Number of 10s: {onezero}")
+print(f"Number of 01s:{zeroone}")
+print(f"Number of 00s:{zerozero}")
