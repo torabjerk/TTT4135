0168e3b809e4609bfb35c60563b41a7cc5e76369
renateaskevold <renate@spektrum.no>
2021-04-13 12:32:38 +0200

    1e

--- Assignment 3/task_1_data1.py
+++ Assignment 3/task_1_data1.py
@@ -5,6 +5,7 @@ import numpy as np
 data = np.loadtxt("data1.txt")
 #data2 = np.loadtxt("data2.txt")
 #data3 = np.loadtxt("data3.txt")
+content = open('data1.txt','r').read().replace('\n','')
 
 ## Task 1d
 data_len = np.size(data)
@@ -56,6 +57,7 @@ print(f"Number of 11s: {oneone}")
 print(f"Number of 10s: {onezero}")
 print(f"Number of 01s: {zeroone}")
 print(f"Number of 00s: {zerozero}")
+print("\n")
 
 p_oneone = float(oneone)/float(data_len)
 p_onezero = float(onezero)/float(data_len)
@@ -66,6 +68,83 @@ H_2bit = -p_zerozero*np.log2(p_zerozero) - (p_zeroone)*np.log2(p_zeroone) \
 - (p_onezero)*np.log2(p_onezero) -(p_oneone)*np.log2(p_oneone)
 print(f"The entropy is for data1 is {H_2bit}")
 
+n3=3
+data_3bit =[content[i:i+n3] for i in range(0,len(content),n3)]
 
-content = open('data1.txt','r').read().replace('\n','')
-print(content)
+occurence_list_3bit = ["000", "001", "010", "011", "100", "101", "110", \
+"111"]
+
+dict_3bit = {"000": 0,
+ "001": 0,
+ "010": 0,
+ "011": 0,
+ "100": 0,
+ "101": 0,
+ "110": 0,
+ "111": 0,
+}
+for i in range(0, np.size(data_3bit)):
+    key = data_3bit[i]
+    if key in occurence_list_3bit:
+        dict_3bit[key] += 1
+
+for key in dict_3bit:
+    H_3bit -= dict_3bit[key]/np.size(data_3bit)+
+
+
+
+n=4
+data_4bit =[content[i:i+n] for i in range(0,len(content),n)]
+
+occurence_list_4bit = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", \
+"0111", "1000", "1001", "1010","1011", "1100", "1101", "1110", "1111"  ]
+
+dict_4bit = {"0000": 0,
+ "0001": 0,
+ "0010": 0,
+ "0011": 0,
+ "0100": 0,
+ "0101": 0,
+ "0110": 0,
+ "0111": 0,
+ "1000": 0,
+ "1001": 0,
+ "1010": 0,
+ "1011": 0,
+ "1100": 0,
+ "1101": 0,
+ "1110": 0,
+ "1111": 0
+}
+
+dict_4bit_p = {"0000": 0,
+ "0001": 0,
+ "0010": 0,
+ "0011": 0,
+ "0100": 0,
+ "0101": 0,
+ "0110": 0,
+ "0111": 0,
+ "1000": 0,
+ "1001": 0,
+ "1010": 0,
+ "1011": 0,
+ "1100": 0,
+ "1101": 0,
+ "1110": 0,
+ "1111": 0
+}
+
+for i in range(0, np.size(data_4bit)):
+    key = data_4bit[i]
+    if key in occurence_list_4bit:
+        dict_4bit[key] += 1
+
+print("\n")
+print(f"Number of {dict_4bit}")
+
+for key in dict_4bit:
+        dict_4bit_p[key] = dict_4bit[key]/np.size(data_4bit)
+
+print("\n")
+print(f"Number of {dict_4bit_p}")
