ddf672dd5fc52dc1b1b365fae12d82d027c90714
anettefb <anettefb@stud.ntnu.no>
2021-04-16 15:35:59 +0200

    coding gain

--- Assignment 3/task_1h.py
+++ Assignment 3/task_1h.py
@@ -7,18 +7,16 @@ data = np.loadtxt("data3.txt")
 #data = "11100101000000000"
 N = [7, 15, 31, 63, 127]
 
-def run_lenght(data, n):
+def run_lenght_n(data, n):
     len_data = len(data)
     C = ""
 
     if data[0] != 0:
         C += "0"
-
     i = 0
     while i < len_data-1:
         count = 1
         while (i < len_data-1 and data[i] == data[i+1]):
-            #print("hello")
             count += 1
             i+=1
 
@@ -32,9 +30,17 @@ def run_lenght(data, n):
         i+= 1
 
     return C
+
+
 code = []
 for item in N:
-    code.append(run_lenght(data,item))
+    code.append(run_lenght_n(data,item))
+
+coding_gain = {}
+for i in range(len(N)):
+    coding_gain[i] = len(data)/len(item)
+
+print(coding_gain)
 
 #print(code[0])
 #print(len(code))
