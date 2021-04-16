3859f2d0663ec91b837ea3bc1df6e5d8d1ce7970
renateaskevold <renate@spektrum.no>
2021-04-16 15:35:01 +0200

    p2_new

---
+++ Assignment 3/p2_new.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5),
+                                                     (0.5, 0.5, 0.5))])
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+kwargs = {} if device=='cpu' else {'num_workers': 1, 'pin_memory': True}
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, **kwargs)
+
+trainsize = 30000
+valsize = 20000
+
+train_set, val_set = torch.utils.data.random_split(trainset, [trainsize,valsize])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # First 2D convolutional layer, taking in 3 input channel (image),
+        # outputting 6 convolutional features, with a square kernel size of 5
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(5):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0): #trainloader changed to train_loader
+        # get the inputs; data is a list of [inputs, labels]
+        # inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        correct = 0
+        total = 0
+    with torch.no_grad():
+        for data in val_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            # images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+
+    print('[%d] loss: %.3f' %
+            (epoch + 1, running_loss / i))
+    print('Accuracy of the network on the validation set: %d %%'
+          % (100 * correct / total))
+
+print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        # images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%'
+      % (100 * correct / total))
