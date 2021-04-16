9610a937b3967521c474e04a472e0c62aebc5833
renateaskevold <renate@spektrum.no>
2021-04-16 15:25:29 +0200

    test p2

--- Assignment 3/problem2_template.py
+++ Assignment 3/problem2_template.py
@@ -7,10 +7,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
-<<<<<<< HEAD
-=======
-
->>>>>>> 08b80c2395d619f0f97af877fe63005a946546c2
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5),
@@ -27,24 +23,15 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, **kwargs)
-"""
-train_set, val_set = torch.utils.data.random_split(dataset, [trainsize,valsize])
 
-for i, data in enumerate(trainloader, 0):
-    input, label = data[0].to(device), data[1].to(device)
+trainsize = 30000
+valsize = 20000
 
-#50000 trainingset
-#10000 testset
-
-
-print(X_train)
-#print("trainset", trainset)
-#print("trainloader", trainloader)
-"""
-
-X_train, X_val, y_train, y_val = train_test_split(trainloader)
-
-print(X_train)
+train_set, val_set = torch.utils.data.random_split(trainset, [trainsize,valsize])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
@@ -80,7 +67,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 for epoch in range(5):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    for i, data in enumerate(train_loader, 0): #trainloader changed to train_loader
         # get the inputs; data is a list of [inputs, labels]
         # inputs, labels = data
         inputs, labels = data[0].to(device), data[1].to(device)
@@ -96,8 +83,22 @@ for epoch in range(5):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in val_loader:
+                images, labels = data[0].to(device), data[1].to(device)
+                # images, labels = data
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+
     print('[%d] loss: %.3f' %
             (epoch + 1, running_loss / i))
+    print('Accuracy of the network on the validation set: %d %%'
+          % (100 * correct / total))
 
 print('Finished Training')
 
