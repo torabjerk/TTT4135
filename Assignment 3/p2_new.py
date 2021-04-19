a88c8fe095a66810eddafafcdba1d22ac02ac22c
renateask <renateaskevold@gmail.com>
2021-04-19 13:06:29 +0200

    p2_test = p2_new with more layers

--- Assignment 3/p2_new.py
+++ Assignment 3/p2_new.py
@@ -60,7 +60,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.conv3 = nn.Conv2d(16, 32, 5)
+        #self.conv3 = nn.Conv2d(16, 32, 5)
 
         self.dropout = nn.Dropout(0.25)
 
@@ -103,6 +103,11 @@ net.to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for name, param in net.named_parameters():
+    print(name, '\t\t', param.shape)
+
+print("\n")
 
 for epoch in range(10):  # loop over the dataset multiple times
 
---
+++ Assignment 3/p2_test.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5),
+                                                     (0.5, 0.5, 0.5))])
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+print("Device: ",device)
+kwargs = {} if device=='cpu' else {'num_workers': 0, 'pin_memory': True}
+batch_size = 10
+print(kwargs)
+
+print("\n")
+print(f"CUDA avalible: {torch.cuda.is_available()}")
+print(f"Current device {torch.cuda.current_device()}")
+print(f"Device 0 {torch.cuda.device(0)}")
+print(f"Device count {torch.cuda.device_count()}")
+print(f"Device name {torch.cuda.get_device_name(0)}")
+print("\n")
+
+
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, **kwargs)
+
+trainsize = 40000
+valsize = 10000
+
+train_set, val_set = torch.utils.data.random_split(trainset, [trainsize,valsize])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
+                                          shuffle=True, **kwargs)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+class Net(nn.Module):
+    def __init__(self, input_shape=(3,32,32)):
+        super(Net, self).__init__()
+        # First 2D convolutional layer, taking in 3 input channel (image),
+        #input size 32*32*3
+        # outputting 6 convolutional features, with a square kernel size of 5
+        self.conv1 = nn.Conv2d(3, 6, 5) #3 input channels (RGB)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+    
+
+        self.dropout = nn.Dropout(0.25)
+
+        n_size = self._get_conv_output(input_shape)
+
+        self.fc1 = nn.Linear(32, 120)   #self.fc1 = nn.Linear(20 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 65)
+        self.fc4 = nn.Linear(65,10)
+
+    def _get_conv_output(self, shape):
+        batch_size = 1
+        input = torch.autograd.Variable(torch.rand(batch_size, *shape))
+        output_feat = self._forward_features(input)
+        n_size = output_feat.data.view(batch_size, -1).size(1)
+        return n_size
+
+    def _forward_features(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = F.relu(self.conv3(x))
+
+        return x
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = F.relu(self.conv3(x))
+        #print("Shape:",x.shape)
+        x = x.view(-1, 32)    #x = x.view(-1, 20 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+"""
+Size:
+Input: (32x32x3)
+
+Conv1: (32x32x16) filter 5x5x3
+Relu1: (32x32x16)
+Pool1: (16x16x16) size 2x2
+
+Conv2: (16x16x20) filter 5x5x16
+Relu2: (16x16x20)
+Pool2: (8x8x20) size 2x2
+
+Conv3: (8x8x20) filter 5x5x20
+Relu3: (8x8x20)
+Pool2: (4x4x20) size 2x2
+"""
+
+net = Net()
+print(f" \n {net} \n")
+net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for name, param in net.named_parameters():
+    print(name, '\t\t', param.shape)
+
+for epoch in range(10):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0): #trainloader changed to train_loader
+        # get the inputs; data is a list of [inputs, labels]
+        # inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        correct = 0
+        total = 0
+    with torch.no_grad():
+        for data in val_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            # images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+
+    print('[%d] loss: %.3f' %
+            (epoch + 1, running_loss / i))
+    print('Accuracy of the network on the validation set: %d %%'
+          % (100 * correct / total))
+
+print("\n")
+
+print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        # images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%'
+      % (100 * correct / total))
+
+print("\n")
+#####
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+#average_precision = average_precision_score(y_test, correct)
+
+"""
+# For each class
+precision = dict()
+recall = dict()
+average_precision = dict()
+for i in range(n_classes):
+    precision[i], recall[i], _ = precision_recall_curve(Y_test[:, i],
+                                                        y_score[:, i])
+    average_precision[i] = average_precision_score(Y_test[:, i], y_score[:, i])
+
+# A "micro-average": quantifying score on all classes jointly
+precision["micro"], recall["micro"], _ = precision_recall_curve(Y_test.ravel(),
+    y_score.ravel())
+average_precision["micro"] = average_precision_score(Y_test, y_score,
+                                                     average="micro")
+print('Average precision score, micro-averaged over all classes: {0:0.2f}'
+      .format(average_precision["micro"]))
+"""
