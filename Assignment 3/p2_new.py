cf29240da87125d4437cc895c27fc1954c9122d1
renateask <renateaskevold@gmail.com>
2021-04-19 10:26:12 +0200

    p2 with dropout

--- Assignment 3/p2_new.py
+++ Assignment 3/p2_new.py
@@ -55,31 +55,56 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # First 2D convolutional layer, taking in 3 input channel (image),
+        #input size 32*32*3
         # outputting 6 convolutional features, with a square kernel size of 5
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+
+        self.dropout = nn.Dropout(0.25)
+
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)   #self.fc1 = nn.Linear(20 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        self.dropout = nn.Dropout(0.25)
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        self.dropout = nn.Dropout(0.25)
+        #x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 16 * 5 * 5)    #x = x.view(-1, 20 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
+"""
+Size:
+Input: (32x32x3)
+
+Conv1: (32x32x16) filter 5x5x3
+Relu1: (32x32x16)
+Pool1: (16x16x16) size 2x2
+
+Conv2: (16x16x20) filter 5x5x16
+Relu2: (16x16x20)
+Pool2: (8x8x20) size 2x2
+
+Conv3: (8x8x20) filter 5x5x20
+Relu3: (8x8x20)
+Pool2: (4x4x20) size 2x2
+"""
 
 net = Net()
+print(f" \n {net} \n")
 net.to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(25):  # loop over the dataset multiple times
+for epoch in range(10):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0): #trainloader changed to train_loader
@@ -115,6 +140,8 @@ for epoch in range(25):  # loop over the dataset multiple times
     print('Accuracy of the network on the validation set: %d %%'
           % (100 * correct / total))
 
+print("\n")
+
 print('Finished Training')
 
 correct = 0
@@ -130,6 +157,29 @@ with torch.no_grad():
 
 print('Accuracy of the network on the 10000 test images: %d %%'
       % (100 * correct / total))
+
+print("\n")
+#####
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+#average_precision = average_precision_score(y_test, correct)
 
 """
 # For each class
